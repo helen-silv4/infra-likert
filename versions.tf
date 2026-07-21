98d72079cf68ccdc7982318408101ebd500a9053
Helen Silva <helen_silva404@hotmail.com>
2026-07-20 21:33:01 -0300

    feat: add workflow de deploy + corrige caminho do state

--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "infra-likert-tfstate"
-        key    = "terraform.tfstate"
+        key    = "dynamodb/terraform.tfstat"
         region = "us-east-1"
         use_lockfile = true
         encrypt = true
