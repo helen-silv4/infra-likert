1c09fcdf346f2d622e4da4c46278ce1718623201
Helen Silva <helen_silva404@hotmail.com>
2026-07-26 22:59:02 -0300

    fix: aplica formatacao padrao terraform fmt

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket = "infra-likert-tfstate"
-        key    = "dynamodb/terraform.tfstat"
-        region = "us-east-1"
-        use_lockfile = true
-        encrypt = true
-    }
+  backend "s3" {
+    bucket       = "infra-likert-tfstate"
+    key          = "dynamodb/terraform.tfstat"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
